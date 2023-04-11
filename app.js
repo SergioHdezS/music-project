d251d2d87d9a6b24ad203568398038b9523a32e2
SergioHdezS <sergiohdez.dev@gmail.com>
2023-04-10 21:11:07 -0400

    avances conexion API

---
+++ app.js
@@ -0,0 +1,180 @@
+var redirect_uri = "http://127.0.0.1:5500/index.html";
+
+var client_id = "2f1f5d05894d45eead8f79ff9d4c2998";
+var client_secret = "9b55497e270747039dc55ab0e95a2e95";
+
+var access_token = null;
+var refresh_token = null;
+
+const AUTHORIZE = "https://accounts.spotify.com/authorize";
+const TOKEN = "https://accounts.spotify.com/api/token";
+
+function onPageLoad() {
+    /*     client_id = localStorage.getItem("client_id");
+        client_secret = localStorage.getItem("client_secret"); */
+    if (window.location.search.length > 0) {
+        handleRedirect();
+    }
+    else {
+        access_token = localStorage.getItem("access_token");
+        if (access_token == null) {
+            // we don't have an access token so present token section
+            document.getElementById("tokenSection").style.display = 'block';
+        }
+        else {
+            // we have an access token so present device section
+            getPlaylistTop50(access_token, playlist_id);
+        }
+    }
+}
+
+function handleRedirect() {
+    let code = getCode();
+    fetchAccessToken(code);
+    window.history.pushState("", "", redirect_uri); // remove param from url
+}
+
+function getCode() {
+    let code = null;
+    const queryString = window.location.search;
+    if (queryString.length > 0) {
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
+    }
+    return code;
+}
+
+function requestAuthorization() {
+    /* client_id = document.getElementById("clientId").value;
+    client_secret = document.getElementById("clientSecret").value; */
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
+
+    let url = AUTHORIZE;
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url; // Show Spotify's authorization screen
+}
+
+function fetchAccessToken(code) {
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
+
+function refreshAccessToken() {
+    refresh_token = localStorage.getItem("refresh_token");
+    let body = "grant_type=refresh_token";
+    body += "&refresh_token=" + refresh_token;
+    body += "&client_id=" + client_id;
+    callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse() {
+    if (this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if (data.access_token != undefined) {
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if (data.refresh_token != undefined) {
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+const playlist_id = '37i9dQZEVXbMDoHDwVN2tF';
+
+/* function getPlaylistTop50(access_token, playlist_id) {
+    const url = `https://api.spotify.com/v1/playlists/${playlist_id}`;
+
+    return fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${access_token}`
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al obtener los datos de la API de Spotify');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const playlist = data;
+            const tracks = playlist.tracks.items.map(item => {
+                const track = item.track;
+                return {
+                    name: track.name,
+                    artist: track.artists[0].name,
+                    album: track.album.name,
+                    image: track.album.images[0].url
+                };
+            });
+            return console.log({
+                name: playlist.name,
+                tracks: tracks
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+} */
+
+function getPlaylistTop50(access_token, playlist_id) {
+    const url = `https://api.spotify.com/v1/playlists/${playlist_id}`;
+
+    return fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${access_token}`
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al obtener los datos de la API de Spotify');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const playlist = data;
+            const tracks = playlist.tracks.items.map(item => {
+                const track = item.track;
+                return {
+                    name: track.name,
+                    artist: track.artists[0].name,
+                    album: track.album.name,
+                    image: track.album.images[0].url
+                };
+            });
+            const playlistData = {
+                name: playlist.name,
+                tracks: tracks
+            };
+            return playlistData; // devolver los datos de la lista de reproducción
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
